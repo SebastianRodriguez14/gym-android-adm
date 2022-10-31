98e249e9f0faa68c0856a880cb7acc66e5f72b95
Oscar <oscartaco2003@gmail.com>
2022-10-30 20:49:37 -0500

    [function] - registar user by admin

---
+++ app/src/main/java/com/tecfit/gym_android_adm/models/UserCustom.kt
@@ -0,0 +1,5 @@
+package com.tecfit.gym_android_adm.models
+
+data class UserCustom( val email:String, val password:String, val name:String, val lastname:String,
+    val phone:String,val image: File
+) {}
--- app/src/main/java/com/tecfit/gym_android_adm/retrofit/ApiService.kt
+++ app/src/main/java/com/tecfit/gym_android_adm/retrofit/ApiService.kt
@@ -1,9 +1,13 @@
 package com.tecfit.gym_android_adm.retrofit
 
+import com.tecfit.gym_android_adm.models.Membership
 import com.tecfit.gym_android_adm.models.Trainer
 import com.tecfit.gym_android_adm.models.User
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import java.lang.reflect.Member
 
 
 interface ApiService {
@@ -13,5 +17,8 @@ interface ApiService {
 
     @GET("trainer/all")
     fun getTrainers(): Call<List<Trainer>>
+
+    @POST("membership/saveWithUser")
+    fun saveMembershipWithUser(@Body membership: Membership): Call<Membership>
 
 }

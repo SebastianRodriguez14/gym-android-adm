848b359bdbd44fb4e1b7550e7ee8bc4e1fcea2fb
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-11-22 20:09:46 -0500

    add layout for register product

--- app/src/main/java/com/tecfit/gym_android_adm/models/custom/ArraysForClass.kt
+++ app/src/main/java/com/tecfit/gym_android_adm/models/custom/ArraysForClass.kt
@@ -1,12 +1,15 @@
 package com.tecfit.gym_android_adm.models.custom
 
 import com.tecfit.gym_android_adm.models.Product
+import com.tecfit.gym_android_adm.models.Trainer
 
 
 class ArraysForClass {
 
     companion object{
-        var arrayProducts: List<Product>? = null
+        var arrayProducts: MutableList<Product> = mutableListOf()
+        var arrayTrainers: MutableList<Trainer> = mutableListOf()
+
     }
 
 }
